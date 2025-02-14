0c47d835360350433578a88ca1da55220d9549c0
arleyhr <arley.hr@hotmail.com>
2025-02-13 21:41:35 -0500

    fix deploy

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,20 +5,8 @@ terraform {
       version = "~> 3.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "terraform-state-rg"
-    storage_account_name = "tfstate${random_string.storage_account_name.result}"
-    container_name       = "tfstate"
-    key                 = "terraform.tfstate"
-  }
 }
 
 provider "azurerm" {
   features {}
-}
-
-resource "random_string" "storage_account_name" {
-  length  = 8
-  special = false
-  upper   = false
 }
