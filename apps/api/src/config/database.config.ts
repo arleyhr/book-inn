fb4b6ce2d46a525b55daabe7f19eb75ec356497b
arleyhr <arley.hr@hotmail.com>
2025-02-14 19:22:59 -0500

    fix deploy

--- apps/api/src/config/database.config.ts
+++ apps/api/src/config/database.config.ts
@@ -7,12 +7,21 @@ import { Reservation } from '../modules/reservations/entities/reservation.entity
 
 const databaseUrl = process.env.DATABASE_URL;
 
+const ssl = {
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
+}
 const databaseConfig = {
   host: process.env.DB_HOST || process.env.AZURE_MYSQL_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT || process.env.AZURE_MYSQL_PORT || '3306', 10),
   username: process.env.DB_USERNAME || process.env.AZURE_MYSQL_USER || 'root',
   password: process.env.DB_PASSWORD || process.env.AZURE_MYSQL_PASSWORD || '',
   database: process.env.DB_NAME || process.env.AZURE_MYSQL_DATABASE || 'book_inn',
+  ...(process.env.AZURE_MYSQL_SSL ? ssl : {})
 }
 
 const dabaseConfigUrl = {
