d0acb4b3ee9851a890c08eb67f90ad1c20ca84e5
arleyhr <arley.hr@hotmail.com>
2025-02-17 20:45:37 -0500

    fix temp images volume

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,11 +6,17 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  app.useStaticAssets(join(process.cwd(), 'uploads'), {
+    prefix: '/uploads/',
+  });
 
   app.enableCors({
     origin: true,
